faf18d1cdf3d5c3ffa36f4c38176d75f3d1a26f7
Carlo Di Cicco <cdicicco2001@gmail.com>
2024-07-12 15:23:47 +0200

    è disponibile la classe FaultTolerance che controlla gli heartbeat ricevuti dai vari nodi e segnala al coordinator eventuali failure.

---
+++ fault_tolerance.py
@@ -0,0 +1,94 @@
+import argparse
+import logging
+import requests
+import threading
+import time
+
+from flask import Flask, request, jsonify
+
+# Configura il logging
+logging.basicConfig(level=logging.INFO)
+
+
+class FaultTolerance:
+    def __init__(self, address, all_nodes, coordinator_address):
+        self.address = address
+        self.all_nodes = all_nodes  # lista di tutti gli id dei nodi
+        self.coordinator_address = coordinator_address
+        self.confirmed_failures = set()
+        self.lock = threading.Lock()
+        self.heartbeat_table = {node_id: time.time() for node_id in self.all_nodes}  # Inizializzare heartbeat_table con node_id come chiave e 0 come valore
+
+    def update_heartbeat_table(self, node_id, timestamp):
+        with self.lock:
+            self.heartbeat_table[node_id] = timestamp
+            print(f"Heartbeat updated for {node_id} at {timestamp}")
+
+    def check_heartbeat_table(self):
+        while True:
+            with self.lock:
+                current_time = time.time()
+                for node_id, timestamp in self.heartbeat_table.items():
+                    if current_time - timestamp > 25:
+                        print(f"NODE {node_id} HAS FAILED")
+                        # self.notify_coordinator(node_id)
+            time.sleep(1)
+
+    def notify_coordinator(self, node_id):
+        try:
+            response = requests.post(f"http://{self.coordinator_address}/node_failure", json={"node_id": node_id})
+            response.raise_for_status()  # Raise an exception for 4xx/5xx status codes, se l'eccezione viene sollevata allora si passa direttamente al blocco except, ALTRIMENTI si va avanti nel blocco try
+            print(f"Coordinator notified of node {node_id} failure")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to notify coordinator: {e}")
+
+    def start(self):
+        app = Flask(__name__)
+
+        @app.route('/heartbeat', methods=['POST'])
+        def report_heartbeat():
+            print("FUNZIONE DI REPORT CHIAMATA")
+            data = request.get_json()
+            node_id = data['node_id']
+            timestamp = data['timestamp']
+            self.update_heartbeat_table(node_id, timestamp)
+            print("FUNZIONE DI UPDATE CHIAMATA")
+            return jsonify({"status": "ok"})
+
+        # Log the server start and listen address
+        logging.info(f"Starting Flask server on {self.address}")
+
+        # Start the heartbeat check in a separate thread
+        threading.Thread(target=self.check_heartbeat_table, daemon=True).start()
+
+        logging.info("Heartbeat check & flask server started for fault tolerance node")
+
+        try:
+            # Start the Flask app in the main thread (this will block the main thread)
+            app.run(host=self.address.split(':')[0], port=int(self.address.split(':')[1]))
+        except Exception as e:
+            logging.error(f"ERROR IN FLASK APP: {e}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Fault Tolerance Node")
+    parser.add_argument('--address', required=True, help='The address of the fault tolerance node (IP:port)')
+    parser.add_argument('--all_nodes', required=True, type=str, help='A JSON string representing a list of all node IDs')
+    parser.add_argument('--coordinator_address', required=True, help='The address of the coordinator node (IP:port)')
+
+    args = parser.parse_args()
+
+    # Parse the all_nodes argument from comma-separated string to list
+    all_nodes = args.all_nodes.split(',')
+
+    fault_tolerance = FaultTolerance(
+        address=args.address,
+        all_nodes=all_nodes,
+        coordinator_address=args.coordinator_address
+    )
+
+    try:
+        fault_tolerance.start()
+        print("Fault tolerance node started")
+    except KeyboardInterrupt:
+        pass  # Handle the interrupt gracefully
